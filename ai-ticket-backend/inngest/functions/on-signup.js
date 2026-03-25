1e2c666fbb1b4b69eaf49a5c003a4b740ddcdca4
Sumit Gupta <cricsumit24@gmail.com>
2026-03-25 22:54:18 +0530

    implement inngest pipeline

---
+++ ai-ticket-backend/inngest/functions/on-signup.js
@@ -0,0 +1,35 @@
+import { inngest } from "../client.js";
+import {User} from "../../models/user.model.js"
+import { NonRetriableError } from "inngest";
+import { sendMail } from "../../utils/mailer.js";
+
+export const onUserSignup = inngest.createFunction(
+    {id: "on-user-signup", retries: 2},
+    {event: "user/signup"},
+    async (event, step) => {
+        try {
+            const {email} = event.data
+            const user = await step.run("get-user-email", async () => {
+                const userObject = await User.findOne({email})
+
+                if(!userObject){
+                    throw new NonRetriableError("User no longer exists")
+                }
+                return user
+            })
+
+            await step.run("send-welcome-email", async () => {
+                const subject = `Welcome to the app`
+                const message = `Hey
+                \n\n
+                Thanks for signing up. We're glad to have you`
+
+                await sendMail(user.email, subject, message)
+            })
+
+            return {success: true}
+        } catch (error) {
+            console.error("Error running step", error.message)
+        }
+    }
+)
