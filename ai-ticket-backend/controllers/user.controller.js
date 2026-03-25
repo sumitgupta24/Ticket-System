75026c5f674922f0f731d514d9070b1317b06ed6
Sumit Gupta <cricsumit24@gmail.com>
2026-03-25 23:12:09 +0530

    implement signup, login and logout controller

---
+++ ai-ticket-backend/controllers/user.controller.js
@@ -0,0 +1,77 @@
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import { inngest } from "../inngest/client.js";
+
+export const signup = async (req, res) => {
+    const { email, password, skills = [] } = req.body
+
+    try {
+        const hashed = brcypt.hash(password, 10)
+
+        const user = User.create({
+            email,
+            password: hashed,
+            skills
+        })
+
+        await inngest.send({
+            name: "user/signup",
+            data: {
+                email
+            }
+        });
+
+        const token = jwt.sign({
+            _id: user._id,
+            role: user.role
+        }, process.env.JWT_SECRET)
+
+        const createdUser = await User.findById(user._id).select("-password")
+
+        return res.status(200).json({
+            createdUser, token
+        })
+    } catch (error) {
+        res.status(500).json({ error: "Signup failed", details: error.message });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = User.findOne({ email }).select("-password");
+        if (!user) return res.status(401).json({ error: "User not found" });
+
+        const isMatch = await brcypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { _id: user._id, role: user.role },
+            process.env.JWT_SECRET
+        );
+
+        return res.status(200).json({ user, token });
+    } catch (error) {
+        return res.status(500).json({ error: "Login failed", details: error.message });
+    }
+};
+
+export const logout = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) return res.status(401).json({ error: "Unauthorzed" });
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) return res.status(401).json({ error: "Unauthorized" });
+        });
+
+        return res.json({ message: "Logout successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: "Login failed", details: error.message });
+    }
+};
--- ai-ticket-backend/inngest/functions/on-signup.js
+++ ai-ticket-backend/inngest/functions/on-signup.js
@@ -1,18 +1,18 @@
 import { inngest } from "../client.js";
-import {User} from "../../models/user.model.js"
+import { User } from "../../models/user.model.js"
 import { NonRetriableError } from "inngest";
 import { sendMail } from "../../utils/mailer.js";
 
 export const onUserSignup = inngest.createFunction(
-    {id: "on-user-signup", retries: 2},
-    {event: "user/signup"},
+    { id: "on-user-signup", retries: 2 },
+    { event: "user/signup" },
     async (event, step) => {
         try {
-            const {email} = event.data
+            const { email } = event.data
             const user = await step.run("get-user-email", async () => {
-                const userObject = await User.findOne({email})
+                const userObject = await User.findOne({ email })
 
-                if(!userObject){
+                if (!userObject) {
                     throw new NonRetriableError("User no longer exists")
                 }
                 return user
@@ -27,7 +27,7 @@ export const onUserSignup = inngest.createFunction(
                 await sendMail(user.email, subject, message)
             })
 
-            return {success: true}
+            return { success: true }
         } catch (error) {
             console.error("Error running step", error.message)
         }
